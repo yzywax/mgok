7e0cca8d610cd9bcd2c00151cb15c8bcefbdd66a
yzywax <borzov1405@gmail.com>
2022-11-25 20:19:59 +0300

    mgok
    mgok

---
+++ Массивы/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] num = new int[5];
+        num[0]= 34;
+        num[1] = 20;
+        num[2] = 3;
+        num[3] = 11;
+        num[4] = 1;
+
+            for (int i = 0; i < num.length;i++) {
+                System.out.println("Элемент " + num[i]);
+
+
+            }
+    }
+}
---
+++ динамический массив(не получилось)/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.ArrayList;
+
+// тут я не смог :((
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+
+            for(int i = 0; i <100;i++) {
+                arrayList.add(i);
+            }
+             System.out.println(arrayList);
+
+               System.out.println( arrayList.get(0));
+               System.out.println( arrayList.get(78));
+            }
+    }
+
